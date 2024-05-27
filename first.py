c355dd1de92fadc210926b28c33316184b836403
Raz-KI <kinginfinitycodes@gmail.com>
2024-05-27 14:30:41 +0530

    ini

---
+++ first.py
@@ -0,0 +1,17 @@
+
+import os
+import torch
+from datasets import load_dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    HfArgumentParser,
+    TrainingArguments,
+    pipeline,
+    logging,
+)
+from peft import LoraConfig, PeftModel
+from trl import SFTTrainer
+
+print(os.time())
